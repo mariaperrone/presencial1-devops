6ce29df2be33e7eac155683eab6b6a8f8e4d6f82
Juan Raimondo <juan.raimondo@estudiantes.utec.edu.uy>
2024-09-11 18:17:11 -0300

    Arreglo del ejercicio 7

--- ejercicio7.py
+++ ejercicio7.py
@@ -9,8 +9,13 @@ def posearch():
     arreglo1 = json.loads(request.args.get('arreglo1'))
     arreglo2 = json.loads(request.args.get('arreglo2'))
     desordenado = arreglo1 + arreglo2
-    resultado = []
-    resultado = sorted(desordenado)
+    resultado = desordenado[:]
+    n = len(resultado)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if resultado[j] > resultado[j + 1]:
+                # Intercambia los elementos
+                resultado[j], resultado[j + 1] = resultado[j + 1], resultado[j]
     
     return jsonify({"arreglo": resultado})
 
